04be4f6977beeceb0e1b98a62954344240c02c24
oscarfloress <oscar.flores@alumno.udg.mx>
2015-03-16 16:42:26 -0600

    Atributos y constructor para ClasePrincipal

--- Practica_3_HashTable/Program.cs
+++ Practica_3_HashTable/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections;
 
 namespace Practica_3_HashTable
 {
     class ClasePrincipal
     {
+        bool salir;
+        private Hashtable hashtable;
+        private Alumno alumno;
+
+        ClasePrincipal()
+        {
+            salir = false;
+            hashtable = new Hashtable();
+            alumno = new Alumno();
+        }
+
         static void Main(string[] args)
         {
         }
