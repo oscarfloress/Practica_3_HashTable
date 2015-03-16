01905a11235ce87ac46e231fe068c9f011ee0647
oscarfloress <oscar.flores@alumno.udg.mx>
2015-03-16 16:39:59 -0600

    Clase Alumno con metodos get y set para nombre y codigo

--- Practica_3_HashTable/Alumno.cs
+++ Practica_3_HashTable/Alumno.cs
@@ -4,5 +4,31 @@ namespace Practica_3_HashTable
 {
     class Alumno
     {
+        public string nombre;
+        public string codigo;
+
+        public Alumno()
+        {
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public string getCodigo()
+        {
+            return codigo;
+        }
+
+        public void setCodigo(string codigo)
+        {
+            this.codigo = codigo;
+        }
     }
 }
